66f765500bf82d704ca441e9114953a5542a6365
H <h@Hs-MacBook-Air.local>
2023-03-23 21:47:06 +0000

    plan_route with map

--- bike-buddy-2.0/src/pages/planroute/PlanRoute.js
+++ bike-buddy-2.0/src/pages/planroute/PlanRoute.js
@@ -1,10 +1,44 @@
 import "./planroute.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import ReactMapGl, { Marker, NavigationControl } from "react-map-gl";
+import { useState } from "react";
 
 const PlanRoute = () => {
+  const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+  const [viewport, setViewport] = useState({
+    latitude: 51.5072,
+    longitude: 0.1276,
+    zoom: 10,
+  });
+
+  const handleViewportChange = (viewport) => {
+    // Update the viewport state immediately
+    setViewport(viewport);
+
+    // Wait for 500 milliseconds before executing the rest of the code
+    setTimeout(() => {
+      console.log("Viewport changed:", viewport);
+    }, 500);
+  };
+
   return (
-    <>
-      <h1>Plan Route</h1>
-    </>
+    <ReactMapGl
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      mapboxAccessToken={MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/hal888/clfl4vcde00eq01o4hy3yymbs"
+      interactiveLayerIds={["my-layer-id"]}
+      dragPan={true}
+      scrollZoom={true}
+      onViewportChange={handleViewportChange}
+    >
+      <NavigationControl />
+
+      <Marker latitude={51.5072} longitude={0.1276}>
+        <div>I'M HERE</div>
+      </Marker>
+    </ReactMapGl>
   );
 };
 
